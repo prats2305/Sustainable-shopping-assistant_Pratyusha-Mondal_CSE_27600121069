3719ec84c0dbd7e4b67d505632dc7197a6bc9868
Pratyusha Mondal <150360243+prats2305@users.noreply.github.com>
2024-12-03 12:17:53 +0530

    Update Impact.js

--- backend/models/Impact.js
+++ backend/models/Impact.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const impactSchema = new mongoose.Schema({
+  userId: mongoose.Schema.Types.ObjectId, // Reference to a User schema (if needed)
+  totalPurchases: Number,
+  co2Saved: Number,
+});
+
+const Impact = mongoose.model('Impact', impactSchema);
+module.exports = Impact;
